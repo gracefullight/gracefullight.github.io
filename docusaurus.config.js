744473a089aa75aa196929f48e918c141acb6335
gracefullight <gracefullight.dev@gmail.com>
2023-01-09 20:42:36 +0900

    ci: github actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gracefullight', // Usually your GitHub org/user name.
   projectName: 'gracefullight.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
