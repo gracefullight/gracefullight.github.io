6f64183e5c46821674eb89c81950595c5e4c8239
gracefullight <gracefullight.dev@gmail.com>
2023-01-10 23:22:20 +0900

    chore: code style

--- src/component/BlogArchiveDescendingPage.tsx
+++ src/component/BlogArchiveDescendingPage.tsx
@@ -1,11 +1,11 @@
 // ? https://johnnyreilly.com/2021/05/01/blog-archive-for-docusaurus
 // ? https://github.com/facebook/docusaurus/blob/main/packages/docusaurus-theme-classic/src/theme/BlogArchivePage/index.tsx
-import React from 'react';
-import Link from '@docusaurus/Link';
-import { translate } from '@docusaurus/Translate';
-import { PageMetadata } from '@docusaurus/theme-common';
-import Layout from '@theme/Layout';
-import type { ArchiveBlogPost, Props } from '@theme/BlogArchivePage';
+import React from "react";
+import Link from "@docusaurus/Link";
+import { translate } from "@docusaurus/Translate";
+import { PageMetadata } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
+import type { ArchiveBlogPost, Props } from "@theme/BlogArchivePage";
 
 type YearProp = {
   year: string;
@@ -49,7 +49,7 @@ function listPostsByYears(blogPosts: readonly ArchiveBlogPost[]): YearProp[] {
   // * reduce: descending
   // * reduceRight: ascending
   const postsByYear = blogPosts.reduce((posts, post) => {
-    const year = post.metadata.date.split('-')[0]!;
+    const year = post.metadata.date.split("-")[0]!;
     const yearPosts = posts.get(year) ?? [];
     return posts.set(year, [post, ...yearPosts]);
   }, new Map<string, ArchiveBlogPost[]>());
@@ -62,14 +62,14 @@ function listPostsByYears(blogPosts: readonly ArchiveBlogPost[]): YearProp[] {
 
 export default function BlogArchive({ archive }: Props): JSX.Element {
   const title = translate({
-    id: 'theme.blog.archive.title',
-    message: 'Archive',
-    description: 'The page & hero title of the blog archive page',
+    id: "theme.blog.archive.title",
+    message: "Archive",
+    description: "The page & hero title of the blog archive page",
   });
   const description = translate({
-    id: 'theme.blog.archive.description',
-    message: 'Archive',
-    description: 'The page & hero description of the blog archive page',
+    id: "theme.blog.archive.description",
+    message: "Archive",
+    description: "The page & hero description of the blog archive page",
   });
   const years = listPostsByYears(archive.blogPosts);
   return (
