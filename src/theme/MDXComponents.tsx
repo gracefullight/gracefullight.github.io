0b6db27fdf6ccdac0d0ea9664ae237e6aee80691
gracefullight <gracefullight.dev@gmail.com>
2024-05-07 19:21:22 +0900

    feat: ✨ iconify-react

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,8 @@
+import { Icon } from "@iconify/react";
+import MDXComponents from "@theme-original/MDXComponents";
+
+// ? https://github.com/facebook/docusaurus/blob/main/website/src/theme/MDXComponents.tsx
+export default {
+  ...MDXComponents,
+  IIcon: Icon,
+};
