ae25b5ed654d8b15da1a7e1439ce59daf09c09c1
gracefullight <gracefullight.dev@gmail.com>
2024-06-22 16:37:45 +0900

    feat: ✨ oxlint with biome

--- config/lint-staged.config.mjs
+++ config/lint-staged.config.mjs
@@ -1,4 +1,7 @@
 export default {
-  "**/*.{ts,tsx,md,mdx}": ["eslint --quiet --fix"],
+  "**/*.{ts,tsx,js,mjs,mts}": "oxlint --fix",
+  "**/*.{js,jsx,mjs,cjs,ts,tsx,yml,json,html,md}": [
+    "biome format --write --no-errors-on-unmatched",
+  ],
   "package.json": "sort-package-json",
 };
