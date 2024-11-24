970719154db05d1c1e195999b7253fe1f46e7f1d
gracefullight <gracefullight.dev@gmail.com>
2024-11-24 18:56:55 +0900

    feat: ✨ github icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -233,8 +233,9 @@ const config: Config = {
         },
         {
           href: "https://github.com/gracefullight",
-          label: "GitHub",
           position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
