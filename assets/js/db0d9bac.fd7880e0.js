66e8a9cff2245d49c9744309ec7fe78140d168e1
gracefullight <gracefullight.dev@gmail.com>
2025-01-13 13:18:16 +0000

    Deploy website - based on b378e7c3ad65069ec4dc6a0668caf2e35be2014a

--- assets/js/db0d9bac.fd7880e0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgracefullight_github_io=self.webpackChunkgracefullight_github_io||[]).push([[58376],{21653:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/page/20","page":20,"postsPerPage":10,"totalPages":65,"totalCount":643,"previousPage":"/page/19","nextPage":"/page/21","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ en/assets/js/7f1e187d.3503fb61.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgracefullight_github_io=self.webpackChunkgracefullight_github_io||[]).push([[48808],{13463:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/en/page/4","page":4,"postsPerPage":10,"totalPages":65,"totalCount":644,"previousPage":"/en/page/3","nextPage":"/en/page/5","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
